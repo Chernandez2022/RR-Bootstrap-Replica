856cba752b0281f930954577916ae9afebfeff7f
Chernandez2022 <hcarlos0816@gmail.com>
2022-12-24 20:27:26 -0500

    Completed background image styling

--- src/componentes/BackgroundImage.js
+++ src/componentes/BackgroundImage.js
@@ -7,14 +7,54 @@ export default function Background() {
       <div>
         <Card
           style={{
-            width: "1000px",
+            width: "1300px",
             minHeight: "420px",
             marginBottom: "50px",
             backgroundColor: "#DEEED6",
           }}
           className="black-text"
-        ></Card>
-        <div></div>
+        >
+          <div
+            style={{
+              top: "auto",
+              width: "570px",
+              marginLeft: "100px",
+              marginBottom: "50px",
+              marginTop: "75px",
+            }}
+          >
+            <Card.text style={{ fontSize: "2.5em", fontwight: "bold" }}>
+              Order Groceries for delivery <br /> Or pickup
+            </Card.text>
+            <Card.Text style={{ fontSize: "1em" }}>
+              Anything you wish from your local stores right to your door.
+            </Card.Text>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.control
+                  size="lg"
+                  type="text"
+                  placeholder="Enter Address"
+                />
+              </Form.Group>
+            </Form>
+          </div>
+        </Card>
+        <div>
+          <img
+            style={{
+              height: "420px",
+              width: "20%",
+              position: "absolute",
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+            }}
+            scr="https://d2d8wwwkmhfcva.cloudfront.net/1500x1520/d2guulkeunn7d8.cloudfront.net/assets/homepage/homepage-mobile-background-m3-high-res-ca3279087ea0a3337f671b694b196d43fd207443d5825e61295b0996974835e9.jpg"
+          />
+        </div>
       </div>
     </div>
   );
