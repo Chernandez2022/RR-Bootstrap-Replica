6be470a99b7c7abcddce7df261d99244822f198d
Chernandez2022 <hcarlos0816@gmail.com>
2022-12-26 22:42:50 -0500

    finishing touches to storelist and images for nav

--- src/componentes/StoreListing.js
+++ src/componentes/StoreListing.js
@@ -5,44 +5,73 @@ import Col from "react-bootstrap/Col";
 
 export default function StoreListings() {
   return (
-    <Container style={{ marginTop: "40px", textAlign: "center" }}>
-      <Row
+    <div style={{ margin: "auto" }}>
+      <h2
         style={{
-          borderBottom: "1px solid #E8E9EB",
-          display: "flex",
-          justifyContent: "space-evenly",
+          fontSize: "36px",
+          lineHeight: "50px",
+          marginTop: "20px",
+          textAlign: "center",
+          fontWeight: "normal",
         }}
       >
-        <Row>
-          <Col xs=".5">
-            <Image src="[IMG SRC]" roundedCircle />
-          </Col>
-          <Col>
-            <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
-            <p>Delivery * Pickup</p>
-          </Col>
+        Browse Stores in Houston
+      </h2>
+
+      <Container style={{ marginTop: "40px", textAlign: "center" }}>
+        <Row
+          style={{
+            borderBottom: "1px solid #E8E9EB",
+            display: "flex",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Row>
+            <Col xs=".5">
+              <Image
+                style={{ height: "55px", width: "50px", border: "1px solid" }}
+                src="https://corporate.aldi.us/fileadmin/_processed_/7/6/csm_aldi_logo_2017_cbae513198.jpg"
+                roundedCircle
+              />
+            </Col>
+            <Col style={{ textAlign: "left" }}>
+              <h5 style={{ fontWeight: "bold" }}>ALDI</h5>
+              <p>Delivery * Pickup</p>
+            </Col>
+          </Row>
+          <Col sm="1"></Col>
+          <Row>
+            <Col xs=".5">
+              <Image
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaeo-HogeAxKLSjLFl1Cm0uheCIDsbJRuDTQ&usqp=CAU "
+                roundedCircle
+              />
+            </Col>
+            <Col md="6">
+              <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
+              <p>Delivery * Pickup</p>
+            </Col>
+          </Row>
+          <Col sm="1"></Col>
+          <Row>
+            <Col xs=".5">
+              <Image
+                style={{
+                  height: "50px",
+                  width: "50px",
+                  border: "1px solid black",
+                }}
+                src="https://www.pngitem.com/pimgs/m/145-1457218_kroger-logo-png-kroger-pharmacy-transparent-png.png"
+                roundedCircle
+              />
+            </Col>
+            <Col md="6">
+              <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
+              <p>Delivery</p>
+            </Col>
+          </Row>
         </Row>
-        <Col sm="1"></Col>
-        <Row>
-          <Col xs=".5">
-            <Image src="[IMG SRC]" roundedCircle />
-          </Col>
-          <Col>
-            <h5 style={{ fontWeight: "bold" }}>Sprouts Farmers Market</h5>
-            <p>Delivery * Pickup</p>
-          </Col>
-        </Row>
-        <Col sm="1"></Col>
-        <Row>
-          <Col xs=".5">
-            <Image src="[IMG SRC]" roundedCircle />
-          </Col>
-          <Col md="6">
-            <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
-            <p>Delivery</p>
-          </Col>
-        </Row>
-      </Row>
-    </Container>
+      </Container>
+    </div>
   );
 }
--- src/componentes/Topbar.js
+++ src/componentes/Topbar.js
@@ -6,12 +6,20 @@ export default function TopBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Brand style={{ margin: "5px 0 0 20px" }} href="#home">
-        <img src="[Instacart logo src]" />
+        <img src="https://images.squarespace-cdn.com/content/v1/5d9f76ad58a4874af8e463b4/1586534119294-5IBOSU4B1NALRFQBQXDD/instacart+logo.png" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto"></Nav>
-        <Nav>
+        <Nav
+          style={{
+            padding: "30px 40px 20px 40px",
+            fontSize: "18px",
+            lineHeight: "26px",
+            fontWeight: "bolder",
+            display: "flex",
+          }}
+        >
           <Nav.Link href="#LogIn">Log In</Nav.Link>
           <Button variant="success">Sign Up</Button>{" "}
         </Nav>
